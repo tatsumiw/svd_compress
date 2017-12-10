08ee6da561303390e6578ce1e62c13f0fc5f74d7
tatsumiw <34232468+tatsumiw@users.noreply.github.com>
2017-12-10 21:48:44 +0800

    Add files via upload

---
+++ built_model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def restore1(sigma, u, v, K):  # 奇异值、左特征向量、右特征向量
+    m = len(u)
+    n = len(v[0])
+    a = np.zeros((m, n))
+    for k in range(K):
+        uk = u[:, k].reshape(m, 1)
+        vk = v[k].reshape(1, n)
+        a += sigma[k] * np.dot(uk, vk)
+    a[a < 0] = 0
+    a[a > 255] = 255
+    return np.rint(a).astype('uint8')
+
+if __name__ == "__main__":
+    A = Image.open("jacky.jpg", 'r')
+    output_path = r'.\Picture'
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    a = np.array(A)
+    K = 60
+    u_r, sigma_r, v_r = np.linalg.svd(a[:, :, 0])
+    u_g, sigma_g, v_g = np.linalg.svd(a[:, :, 1])
+    u_b, sigma_b, v_b = np.linalg.svd(a[:, :, 2])
+    plt.figure(figsize=(10,10), facecolor='w')
+    mpl.rcParams['font.sans-serif'] = [u'simHei']
+    mpl.rcParams['axes.unicode_minus'] = False
+    i = 1
+    while i <= 16:
+        for k in range(1, K+1):
+            print(k)
+            R = restore1(sigma_r, u_r, v_r, k)
+            G = restore1(sigma_g, u_g, v_g, k)
+            B = restore1(sigma_b, u_b, v_b, k)
+            I = np.stack((R, G, B), 2)
+            Image.fromarray(I).save('%s\\svd_%d.png' % (output_path, k))
+            if k in [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]:
+                plt.subplot(4, 4, i)
+                plt.imshow(I)
+                plt.axis('off')
+                plt.title(u'奇异值数目：%d' % k)
+                i += 1
+    plt.show()
